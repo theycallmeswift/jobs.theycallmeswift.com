0a6af79a8998f350f89fef98344d8aedad0c7ce2
Swift <theycallmeswift@gmail.com>
2013-07-22 21:24:31 -0400

    Better serialization and styles

--- js/app.js
+++ js/app.js
@@ -4,10 +4,12 @@ $(document).ready(function() {
   form.submit(function(e) { e.preventDefault(); });
 
   form.on('valid', function () {
+    var values = form.clone().find("input, textarea").each(function() {  $(this).val() == "" && $(this).remove(); }).end().serialize();
+
     form.hide()
     $('.spinner').show()
 
-    $.post("http://api.theycallmeswift.com/v1/applicants", form.find('input').not('[value=""]').serialize())
+    $.post("http://api.theycallmeswift.com/v1/applicants", values)
       .done(function(data) {
         console.log(data);
         $('#application-form').html('<div class="large-12 small-12 columns"><h2>Thanks for Applying!</h2><p>Your application was successfully submitted, keep an eye on your inbox for updates!</p></div>');
